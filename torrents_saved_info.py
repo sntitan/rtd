d97804cf9d4ae6b358e527f5b537ff5a6d96eae4
titan sheng <sntitan@sntitan-air.local>
2014-04-20 18:39:35 +0800

    修复一些bug

--- torrents_saved_info.py
+++ torrents_saved_info.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
+# coding=utf-8
 import sys
 import sqlite3
 import time
 import ConfigParser
 import feedparser
-import urllib2
+import urllib
 import hashlib
 import os
 import shutil
+from bencode import bdecode
 
 ###config
 config_file_name='tord.ini'
@@ -27,6 +29,7 @@ def create_table(conn, name):
     create_sql = "CREATE TABLE IF NOT EXISTS " + name
     create_sql += " (addr TEXT, fname TEXT, dcnt INT, dtime TIMESTAMP, fsha1 TEXT)"
     sor.execute(create_sql)
+    conn.commit()
 
 def is_table_exists(sor, name):
     tb_list = sor.execute("select name from sqlite_master where type = 'table'").fetchall()
@@ -63,6 +66,7 @@ def insert_torrent(conn, name, info):
     sor=conn.cursor()
     insert_sql="INSERT INTO "+name+" VALUES (?,?,?,?,?)"
     sor.execute(insert_sql,info)
+    conn.commit()
 
 
 #config file adapt
@@ -95,6 +99,22 @@ def get_down_dirs(cfg):
 
 
 #torrents analyser
+def get_name_by_tfile(prefix, feedbuff):
+    try:
+        tor = bdecode(feedbuff)
+    except:
+        print("Invalid feed file.")
+        return None
+    if not tor.has_key('info'):
+        return None
+    if type(tor['info']) != type({}):
+        return None
+    if not tor['info'].has_key('name'):
+        return None
+    if type(tor['info']['name']) != type('str'):
+        return None
+    return '[' + prefix + ']'  + ' ' + tor['info']['name'] + '.torrent'
+
 def calc_sha1(filepath):
     with open(filepath,'rb') as f:
         sha1obj = hashlib.sha1()
@@ -104,57 +124,49 @@ def calc_sha1(filepath):
 
 def download_torrent(url, save_path):
     try:
+        out_debug('Download from %s, save path %s'%(url,save_path))
         urllib.urlretrieve(url,save_path)
-    except:
-        out_err("Download torrent err, addr %s" % url)
+    except Exception, e:
+        out_error("Download torrent err, addr %s" % url)
         return None
     with open(save_path, 'r') as pf:
-        return hashlib.sha256(f.read()).hexdigest()
+        return hashlib.sha256(pf.read()).hexdigest()
     return None
-
-    pf = open(down_full_path,'r')
-    pbuff = pf.read()
-    name = GetNameByDecodeFile(prefix,pbuff)
-    if name == None:
-        out_err("Invalid addr maybe")
-        return
-    with open(name,"wb") as file:
-        file.write(feed)
-    return 'sha1'
 
 def download_torrents_list(db, host_name, addr_list, down_dir):
     for addr_info in addr_list:
         if not addr_info.has_key('href'):
             continue
         addr = addr_info['href']
-        if len(find_torrent_by_addr(db, host_name, addr) != 0):
+        if len(find_torrent_by_addr(db, host_name, addr)) != 0:
+            out_debug('The torrent in addr %s exists'%addr)
             continue
         tmp_torrent_path = os.path.join(down_dir[1],'torrent.tmp')
         if os.path.isfile(tmp_torrent_path):
+            out_debug('File %s exists, delete first'%tmp_torrent_path)
             os.remove(tmp_torrent_path)
         tsha = download_torrent(addr,tmp_torrent_path)
         if (tsha == None):
-            out_err('Download from addr %s fail'%addr)
+            out_error('Download from addr %s fail'%addr)
             continue
         with open(tmp_torrent_path,'r') as f:
-            tor_name = GetNameByDecodeFile(host_name, f.read())
+            tor_name = get_name_by_tfile(host_name, f.read())
         if tor_name == None:
-            out_err("Invalid file format")
+            out_error("Invalid file format")
             continue
         if len(find_torrent_by_hash(db, host_name, tsha)) != 0:
-            out_info('Download file %s but the same hash'%tor_name)
+            out_info('Download file %s but the same hash'%tor_name.decode('utf-8'))
         else:
-            out_info('Download file %s from addr %s'%(tor_name,addr))
-            tor_path = os.path.join(down_dir[0],tor_name)
+            out_info('Download file %s from addr %s'%(tor_name.decode('utf-8'),addr))
+            tor_path = os.path.join(down_dir[0],tor_name.decode('utf-8'))
             shutil.move(tmp_torrent_path,tor_path)
-            os.remove(tmp_torrent_path)
-        insert_torrent(db, host_name,[(addr,tor_name,1,time.strftime('%Y-%m-%d %X', time.localtime()),tsha)])
+        insert_torrent(db, host_name,[addr,tor_name.decode('utf-8'),1,time.strftime('%Y-%m-%d %X', time.localtime()),tsha])
     return
 
 def download_all_torrents(db, host_name, rss_addr, down_dir):
     hd = feedparser.parse(rss_addr)
     for feed in hd.entries:
-        if len(feed.enclosures != 0):
+        if len(feed.enclosures) != 0:
             download_torrents_list(db, host_name, feed.enclosures, down_dir)
         else:
             download_torrents_list(db, host_name, feed.links, down_dir)
@@ -177,7 +189,13 @@ if tdir[0] == '':
     out_error('A download-dir where torrents saved, should be specified')
     exit(0)
 out_debug("Download dir '%s'"%tdir[0])
+if not os.path.exists(tdir[0]):
+    out_info('Directory %s is not exists, create it first'%tdir[0])
+    os.makedirs(tdir[0])
 out_debug("Incomplete download dir '%s'"%tdir[1])
+if not os.path.exists(tdir[1]):
+    out_info('Directory %s is not exists, create it first'%tdir[1])
+    os.makedirs(tdir[1])
 
 #4.Open database and init
 out_debug("database file %s, open"%db_file_name)
